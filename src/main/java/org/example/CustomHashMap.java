03b869e3a364d26115b582e204d00701a0d90324
Viktor <vikt.sh@gmail.com>
2025-07-30 19:19:28 +0300

    Меняем параметры во внутреннем классе Node. Добавляем поле key, исправляем конструктор.

--- src/main/java/org/example/CustomHashMap.java
+++ src/main/java/org/example/CustomHashMap.java
@@ -24,13 +24,15 @@ public class CustomHashMap<K, V> {
         this.size = 0;
     }
 
-    private static class Node<E> {
+    private static class Node<K, V> {
         final int hash;
-        final E value;
-        Node<E> next;
+        final K key;
+        V value;
+        Node<K, V> next;
 
-        Node(int hash, E value, Node<E> next) {
+        Node(int hash,K key, V value, Node<K, V> next) {
             this.hash = hash;
+            this.key = key;
             this.value = value;
             this.next = next;
         }
